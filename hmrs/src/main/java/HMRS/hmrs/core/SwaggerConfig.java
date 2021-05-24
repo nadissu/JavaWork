916328ef1471fc34ca2399547bfd40f84919c35b
nadissu <nadidesigirtmac@gmail.com>
2021-05-24 12:32:08 +0300

    Swagger Config

--- hmrs/src/main/java/HMRS/hmrs/core/SwaggerConfig.java
+++ hmrs/src/main/java/HMRS/hmrs/core/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package HMRS.hmrs.core;
 
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
